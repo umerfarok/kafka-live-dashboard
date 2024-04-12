5959c63da403a96458d8b143a762e24802ffe071
umerfarok <umerfarooq.dev@gmail.com>
2024-04-12 23:04:34 +0500

    Add config package and main.go file

---
+++ go.mod
@@ -0,0 +1,50 @@
+module github.com/umerfarok/kafka-live-dashbord
+
+go 1.21.0
+
+require (
+	github.com/IBM/sarama v1.43.1
+	github.com/gorilla/websocket v1.5.1
+	github.com/samuel/go-zookeeper v0.0.0-20201211165307-7117e9ea2414
+	github.com/spf13/viper v1.18.2
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/eapache/go-resiliency v1.6.0 // indirect
+	github.com/eapache/go-xerial-snappy v0.0.0-20230731223053-c322873962e3 // indirect
+	github.com/eapache/queue v1.1.0 // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-uuid v1.0.3 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
+	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
+	github.com/jcmturner/gofork v1.7.6 // indirect
+	github.com/jcmturner/gokrb5/v8 v8.4.4 // indirect
+	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
+	github.com/klauspost/compress v1.17.7 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	github.com/pierrec/lz4/v4 v4.1.21 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
+	github.com/sagikazarmark/locafero v0.4.0 // indirect
+	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.11.0 // indirect
+	github.com/spf13/cast v1.6.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.9.0 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/net v0.22.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/IBM/sarama"
+	"github.com/gorilla/websocket"
+	"github.com/samuel/go-zookeeper/zk"
+	"github.com/umerfarok/kafka-live-dashbord/config"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+type TopicStatus struct {
+	Name    string
+	Alive   bool
+	Message string
+}
+
+type Server struct {
+	config    *config.Config
+	kafkaConn sarama.Client
+	zkConn    *zk.Conn
+	topics    []string
+	mu        sync.Mutex
+}
+
+func NewServer(config *config.Config) (*Server, error) {
+	kafkaConfig := sarama.NewConfig()
+	kafkaConfig.Version = sarama.V2_6_0_0
+	kafkaConn, err := sarama.NewClient(strings.Split(config.KafkaBrokers, ","), kafkaConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	zkConn, _, err := zk.Connect(strings.Split(config.ZookeeperNodes, ","), time.Second)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Server{
+		config:    config,
+		kafkaConn: kafkaConn,
+		zkConn:    zkConn,
+	}, nil
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		fmt.Fprintf(w, "Hello World")
+		return
+	} else if r.URL.Path == "/topics" {
+		topics := s.GetTopics()
+		jsonBytes, err := json.Marshal(topics)
+		if err != nil {
+			http.Error(w, "Failed to marshal topics", http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(jsonBytes)
+		return
+	} else if r.URL.Path == "/ws" {
+		s.ServeWebSocket(w, r)
+		return
+	}
+
+	http.NotFound(w, r)
+}
+
+func (s *Server) GetTopics() []string {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if s.topics == nil {
+		topics, _, err := s.zkConn.Children("/brokers/topics")
+		if err != nil {
+			log.Println("Failed to get topics from Zookeeper:", err)
+			return nil
+		}
+		s.topics = topics
+	}
+	return s.topics
+}
+
+func (s *Server) ServeWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("WebSocket upgrade error:", err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("WebSocket read error:", err)
+			return
+		}
+		topic := string(msg)
+		s.HandleWebSocket(conn, topic)
+	}
+}
+
+func (s *Server) HandleWebSocket(conn *websocket.Conn, topic string) {
+	consumer, err := sarama.NewConsumerFromClient(s.kafkaConn)
+	if err != nil {
+		log.Println("Failed to create consumer:", err)
+		return
+	}
+	partitionConsumer, err := consumer.ConsumePartition(topic, 0, sarama.OffsetNewest)
+	if err != nil {
+		log.Println("Failed to start consumer for partition:", err)
+		return
+	}
+	defer partitionConsumer.Close()
+
+	for message := range partitionConsumer.Messages() {
+		err := conn.WriteMessage(websocket.TextMessage, message.Value)
+		if err != nil {
+			log.Println("WebSocket write error:", err)
+			return
+		}
+	}
+}
+
+func main() {
+	config, err := config.NewConfig()
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
+	server, err := NewServer(config)
+	if err != nil {
+		log.Fatalf("Failed to create server: %v", err)
+	}
+
+	http.Handle("/", server)
+	log.Printf("Starting server on :%s\n", config.HTTPPort)
+	if err := http.ListenAndServe(":"+config.HTTPPort, nil); err != nil {
+		log.Fatalf("ListenAndServe error: %v", err)
+	}
+}
